5ecc359b3735b40876cd40377e9f706245b3c380
lucascorreaa <lucas.lca14@gmail.com>
2024-06-27 22:06:07 -0300

    feat: created the listing and creation route

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -3,7 +3,41 @@ import http from 'node:http'
 // request = req
 // response = res
 
+// HTTP
+// - method HTTP
+// - url
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso no back-end
+// POST => Criar um recurso no back-end
+// PUT => Editar/Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso no back-end
+
+// GET /users => Buscando usuários no back-end
+// GET /users => Criar um usuário no back-end
+
+
+// Early return => No js dentro de uma função, se o codigo bater no return nada abaixo é executado
+
 const server = http.createServer((req, res) => {
+
+    const { method, url } = req
+    // console.log(method, url)
+
+    if (method === 'GET' && url === '/users') {
+
+        // Early return
+        return res.end('Listagem de usuários')
+    }
+
+    if (method === 'POST' && url === '/users') {
+
+        return res.end('Criação de usuários')
+    }
+    
+
     return res.end('Hello Nodee')
 })
 
