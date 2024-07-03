c540a5b2cd13606710d4a671c69d23242c518982
lucascorreaa <lucas.lca14@gmail.com>
2024-07-02 21:42:15 -0300

    feat: understanding streams on node

---
+++ 01-fundamentos-nodejs/streams/fundamentals.js
@@ -0,0 +1,18 @@
+// streams => node
+
+// Netflix & Spotify
+
+// Importação de clientes via CSV (Excel)
+// 1gb - 100000
+// POST /upload import.csv
+
+// 10mb/s - 10
+
+// 100s => Inserções no banco de dados
+
+// 10mb/s => 10.000
+
+// Readable Streams Lendo aos poucos  
+// Writable Streams = enviando aos poucos
+
+
