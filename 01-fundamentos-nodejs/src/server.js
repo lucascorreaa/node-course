44b51c1b202a3e8ff7153638b49c71067dde2c78
lucascorreaa <lucas.lca14@gmail.com>
2024-07-03 06:23:01 -0300

    feat: request body in JSON (stream & buffers)

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -34,12 +34,22 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
 
     const { method, url } = req
 
-    // console.log(method, url)
-    // console.log(req.headers)
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = null
+    }
+
 
     if (method === 'GET' && url === '/users') {
 
@@ -50,10 +60,11 @@ const server = http.createServer((req, res) => {
     }
 
     if (method === 'POST' && url === '/users') {
+        const { name, email } = req.body
         users.push({
             id: 1,
-            name: 'Fulano',
-            email: 'fulano@example.com'
+            name,
+            email
         })
         return res.writeHead(201).end()
     }
